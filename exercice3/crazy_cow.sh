00b72ff6de202ba86cb6b9752d213c003834d628
Brun-Cosme-Gazot Guillaume <bruncosg@etu.univ-grenoble-alpes.fr>
2022-03-01 12:31:16 +0100

    Nouveautes de ouf !

--- exercice3/crazy_cow.sh
+++ exercice3/crazy_cow.sh
@@ -1,28 +1,3 @@
-#!/bin/bash
-a=1
-b=2
-((c=$a+$b))
-while [[ $c -lt $1 ]]
-do
-    for i in {2..$c}
-    do
-        somme=0
-        for j in {1..$i-1}
-        do
-            if (( i mod j != 1 ))
-            then 
-                ((somme++))
-            fi
-
-            if (($somme == 0))
-            then
-                cowsay $c 
-                sleep 1s
-                clear
-            fi
-        done
-    done
-a=$b
-b=$c
-((c=$a+$b))
-done
+# Nbr random de 1 - x :
+# Si 1 alors nanana
+# Si 2 alors nbnbnb
