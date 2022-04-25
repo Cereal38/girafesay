d3ae36595d91b1a6ecb64979099db6e929e7ba14
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2022-04-25 12:14:57 +0200

    Fix neck

--- C/sources/newgirafe.c
+++ C/sources/newgirafe.c
@@ -14,6 +14,7 @@ int main (int argc, char * argv[]) {
 	// Balaye tout les éléments de argv[]
 	for (int i=1; i<=argc-1; i++) {
 			
+
 		// Si argument est --eyes (ou -e)
 		if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--eyes") == 0) {
 			
@@ -22,6 +23,7 @@ int main (int argc, char * argv[]) {
 			else { printf("[ERREUR] - Bad argument for --eyes\n"); return 1; };
 
 	       	};
+
 
 		// Si argument est --mouth (ou -m)
 		if (strcmp(argv[i], "-m") == 0 || strcmp(argv[i], "--mouth") == 0) {
@@ -34,18 +36,25 @@ int main (int argc, char * argv[]) {
 
 
 		// Si argument est --neck (ou -n)
+		// Converti le char en entier
 		if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--neck") == 0) {
-			
-			// FAIRE UN TRUC IDIOT PROOF POUR LA LONGUEUR DU COUP
-			// TESTER SI ENTIER
 
-			// Converti le char en entier
 			sscanf(argv[i+1], "%i", &neck);
+			if (!(neck <= 40 && neck > 0)) { 
+				
+				printf("ERROR : Neck length too long (%d) | Must be between 0 and 40.\n", neck);
+				neck = 6;
+			}
+		}
+
+		if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--prophunt") == 0) {
+		
+			
 		}
 	}
 
+	
 
-	// AJOUTER DES OPTIONS
 	// PROP HUNT
 	affiche_girafe(eyes, mouth, neck);
 	
