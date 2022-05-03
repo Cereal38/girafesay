dc38db6487f6c93141126b71ed7ad5704d42dec3
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2022-05-03 09:52:13 +0200

    Delete useless files

--- C/backup/backup_affiche_girafe.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void affiche_girafe(char * eyes) {
-	printf ("            o o     /\n	  (\\|_|/)  /\n	  /     \\ /\n	 (  ._.  )\n	  \\     /\n	   \\___/\n            | |\n            | |\n            | |\n            | |\n            | |\n            | |\n	   /   \\\n	  /    #\\\n	 |##     |\n	 |#   ## |\\\n	 \\   _#  / |\n	  | / \\ || w\n	  |||  |||\n	  ||v  ||v\n	  ||   ||\n	  vv   vv\n	");
-}
